0dd459f56fc1a48544c1302d26edb1d9a81fb3fe
kyle <altck8@gmail.com>
2016-02-10 13:30:09 -0800

    added game logic to check for winner and rest the game

--- js/scripts.js
+++ js/scripts.js
@@ -36,33 +36,87 @@ function Game( playerNameOne, playerNameTwo )
     this.players = [new Player( 0, playerNameOne ), new Player( 1, playerNameTwo )];
     this.board = new Board( );
     this.currentPlayer = this.players[ this.move ];
+    this.running = false;
 }
 
 $(document).ready( function( ) 
 {
-    var game = new Game( $("#player-one").val( ), $("#player-two").val( ) );
+    var game;
     var currentPlayer = $("#current-player");
     
-    currentPlayer.text( "Current Player: " + game.currentPlayer.name );    
-    
-    $(".img-thumbnail").on("click", function( event ) {
+   $(".img-thumbnail").on("click", function( event ) {
         var x = parseInt( event.currentTarget.name[0] );
         var y = parseInt( event.currentTarget.name[1] );
             
         var clickedSquare = game.board.getSpaceByLocation( x, y );
         
-        if ( clickedSquare.player == -1 )
+        if ( clickedSquare.player == -1 && game.running )
         {
+            clickedSquare.player = game.currentPlayer.team;
             game.move = ( game.move == 0 ) ? 1 : 0;
             game.currentPlayer = game.players[ game.move ];
             currentPlayer.text( "Current Player: " + game.currentPlayer.name );
             
-            clickedSquare.player = game.currentPlayer.team;
-            
             var imageType = ( game.move == 0 ) ? "img/O.png" : "img/X.png";
             var elementName = "[name=" + event.currentTarget.name + "]";
             $(elementName).attr("src", imageType );
+            
+            var checkWinner = checkForWinner( game.board.spaces );
+            if ( checkWinner != -1 )
+            {
+                $("#header").text( "Winner! - " + game.players[ checkWinner ].name );
+                game.running = false;
+                
+                currentPlayer.text( );
+            }
         }
     });
+    
+    $("#start-game").on("click", function( ) {
+        game = new Game( $("#player-one").val( ), $("#player-two").val( ) );
+        game.running = true;
+        
+        $(".img-thumbnail").each(function( ) {
+            $("[name=" + this.name + "]").attr("src", "img/white.png");
+        });
+        
+        $("#header").text( "Tic Tac Toe" );
+        currentPlayer.text( "Current Player: " + game.currentPlayer.name );   
+    });
+    
+    function checkForWinner( spaces )
+    {
+        var team = spaces[0].player;
+        if( spaces[1].player === team && spaces[2].player === team )
+        {
+            return spaces[0].player;
+        }
+        
+        team = spaces[3].player;
+        if( spaces[4].player === team && spaces[5].player === team )
+        {
+            return spaces[3].player;
+        }
+        
+        team = spaces[6].player;
+        if( spaces[7].player === team && spaces[8].player === team )
+        {
+            return spaces[6].player;
+        }
+        
+        team = spaces[0].player;
+        if( spaces[4].player === team && spaces[8].player === team )
+        {
+            return spaces[0].player;
+        }
+        
+        team = spaces[6].player;
+        if( spaces[4].player === team && spaces[2].player === team )
+        {
+            return spaces[6].player;
+        }
+        
+        return -1;
+    }
 });
 
