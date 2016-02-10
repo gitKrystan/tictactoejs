31c86d2fc935a3aae833762364a72e8a6e77d836
kyle <altck8@gmail.com>
2016-02-10 11:11:16 -0800

    default html grid and img files for game

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,30 @@
-// default
+
+
+function Player( team, name )
+{
+    this.team = team;
+    this.name = name;
+}
+
+function Space( player )
+{
+    this.player = "null";
+}
+
+Space.prototype.setPlayerToSpace = function( player )
+{
+    this.player = player;
+}
+
+function Board( )
+{
+    this.spaces = [[0,0,-1],[0,1,-1][0,2,-1]
+                   [1,0,-1],[1,1,-1],[1,2,-1]
+                   [2,0,-1],[2,1,-1],[2,2,-1]];
+}
+
+function Game( )
+{
+    this.players = [];
+    this.board = new Board( );
+}
