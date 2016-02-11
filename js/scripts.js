0c5b77ac0516a5e0a2cd30b6ab4a54db16fcd1af
Krystan Menne and Nathan Romike <nathanromike@gmail.com>
2016-02-11 09:28:06 -0800

    code review with minor refactors

--- js/scripts.js
+++ js/scripts.js
@@ -1,181 +1,184 @@
-
-
-function Player( team, name, computer )
+function Player(team, name, computer)
 {
-    this.team = team;
-    this.name = name;
-    this.computer = computer;
+  this.team = team;
+  this.name = name;
+  this.computer = computer;
 }
 
-function Space( locationX, locationY, team )
+function Space(locationX, locationY, team)
 {
-    this.player = -1;
-    this.location = [locationX,locationY,team];
+  this.player = -1;
+  this.location = [locationX,locationY,team];
 }
 
-function Board( )
+function Board()
 {
-    this.spaces = [new Space(0,0,-1),new Space(0,1,-1),new Space(0,2,-1),
-                   new Space(1,0,-1),new Space(1,1,-1),new Space(1,2,-1),
-                   new Space(2,0,-1),new Space(2,1,-1),new Space(2,2,-1)];
+  this.spaces = [new Space(0,0),new Space(0,1),new Space(0,2),
+                 new Space(1,0),new Space(1,1),new Space(1,2),
+                 new Space(2,0),new Space(2,1),new Space(2,2)];
 }
 
-Board.prototype.getSpaceByLocation = function( x, y )
+Board.prototype.getSpaceByLocation = function(x, y)
 {
-    for( var i = 0; i < this.spaces.length; i++ )
+  var spaces = this.spaces;
+  for(var i = 0; i < spaces.length; i++)
+  {
+    if(spaces[i].location[0] === x && spaces[i].location[1] === y)
     {
-        if( this.spaces[i].location[0] == x && this.spaces[i].location[1] == y )
-        {
-            return this.spaces[i];
-        }
+      return spaces[i];
     }
+  }
+};
+
+function Game(playerNameOne, playerNameTwo)
+{
+  this.move = 0;
+  this.players = [new Player(0, playerNameOne, false), new Player(1, playerNameTwo, $("#computer").is(":checked"))];
+  this.board = new Board();
+  this.currentPlayer = this.players[this.move];
+  this.running = true;
 }
 
-function Game( playerNameOne, playerNameTwo )
+$(document).ready(function()
 {
-    this.move = 0;
-    this.players = [new Player( 0, playerNameOne, false ), new Player( 1, playerNameTwo, $("#computer").is(":checked") )];
-    this.board = new Board( );
-    this.currentPlayer = this.players[ this.move ];
-    this.running = false;
-}
+  var game;
+  var clickedSquare;
+  var $currentPlayer = $("#current-player");
 
-$(document).ready( function( ) 
-{
-    var game;
-    var currentPlayer = $("#current-player");
-    var clickedSquare;
-    
-   $(".img-thumbnail").on("click", function( event ) {
-        var x = parseInt( event.currentTarget.name[0] );
-        var y = parseInt( event.currentTarget.name[1] );
-            
-        clickedSquare = game.board.getSpaceByLocation( x, y );
-        
-        if ( clickedSquare.player == -1 && game.running )
-        {
-            var imageType = ( game.move == 0 ) ? "img/O.png" : "img/X.png";
-            var elementName = "[name=" + event.currentTarget.name + "]";
-            $(elementName).attr("src", imageType );
-                
-            setupNextMove( );
-            
-            if( game.currentPlayer.computer )
-            {
-                computerMove( );
-            }
-        }
+  // Start the game
+  $("#start-game").on("click", function() {
+    game = new Game($("#player-one").val(), $("#player-two").val());
+
+    // Reset all tiles to white
+    $(".img-thumbnail").each(function() {
+      $("[name=" + this.name + "]").attr("src", "img/white.png");
     });
-    
-    $("#start-game").on("click", function( ) {
-        game = new Game( $("#player-one").val( ), $("#player-two").val( ) );
-        game.running = true;
-        
-        $(".img-thumbnail").each(function( ) {
-            $("[name=" + this.name + "]").attr("src", "img/white.png");
-        });
-        
-        $("#header").text( "Tic Tac Toe" );
-        currentPlayer.text( "Current Player: " + game.currentPlayer.name );   
-    });
-    
-    function setupNextMove( )
+
+    // Reset the text
+    $("#header").text("Tic Tac Toe");
+    $currentPlayer.text("Current Player: " + game.currentPlayer.name);
+  });
+
+  // Game play
+  $(".img-thumbnail").on("click", function(event) {
+    var x = parseInt(this.name[0]);
+    var y = parseInt(this.name[1]);
+
+    clickedSquare = game.board.getSpaceByLocation(x, y);
+
+    // if there is no current player assigned to the clicked square
+    if (clickedSquare.player === -1 && game.running)
     {
-        clickedSquare.player = game.currentPlayer.team;
-         
-        var checkWinner = checkForWinner( game.board.spaces );
-        if ( checkWinner != -1 )
-        {
-           $("#header").text( "Winner! - " + game.players[ checkWinner ].name );
-           game.running = false;
-                    
-           currentPlayer.text( );
-        }
-                
-        game.move = ( game.move == 0 ) ? 1 : 0;
-        game.currentPlayer = game.players[ game.move ];
-        currentPlayer.text( "Current Player: " + game.currentPlayer.name );
+      // if game.move === 0, then it's 'O's turn
+      var imageType = (game.move === 0) ? "img/O.png" : "img/X.png";
+      $(this).attr("src", imageType);
+
+      setupNextMove();
+
+      if(game.currentPlayer.computer)
+      {
+        computerMove();
+      }
     }
-    
-    function computerMove( )
+  });
+
+  function setupNextMove()
+  {
+    clickedSquare.player = game.currentPlayer.team;
+
+    var checkWinner = checkForWinner(game.board.spaces);
+    // If there is a winner...
+    if (checkWinner != -1)
     {
-        if( game.running )
-        {
-            var randomSpace = findRandomSpace( game.board.spaces );
-            var randomSpaceName = randomSpace.location[0].toString( ) + randomSpace.location[1].toString( );
-            var imageType = ( game.move == 0 ) ? "img/O.png" : "img/X.png";
-            
-            clickedSquare = game.board.getSpaceByLocation( randomSpace.location[0], randomSpace.location[1] );
-            $("[name=" + randomSpaceName + "]").attr("src", imageType);
-                
-            setupNextMove( );
-        }
-    }
-    
-    function findRandomSpace( spaces )
+      $("#header").text("Winner! - " + game.players[checkWinner].name);
+      game.running = false;
+
+      $currentPlayer.text();
+    } else
     {
-        var availableSpaces = []
-        for( var i = 0; i < spaces.length; i++ )
-        {
-            if( spaces[ i ].player === -1)
-            {
-                availableSpaces.push( spaces[ i ] );
-            }
-        }
-        return availableSpaces[ Math.floor( Math.random( ) * availableSpaces.length ) ];
+      // Toggle player between player 0 and player 1
+      game.move = (game.move == 0) ? 1 : 0;
+      game.currentPlayer = game.players[game.move];
+      $currentPlayer.text("Current Player: " + game.currentPlayer.name);
     }
-    
-    function checkForWinner( spaces )
+  }
+
+  function computerMove()
+  {
+    var randomSpace = findRandomSpace(game.board.spaces);
+    var randomSpaceName = randomSpace.location[0].toString() + randomSpace.location[1].toString();
+    var imageType = (game.move == 0) ? "img/O.png" : "img/X.png";
+
+    clickedSquare = game.board.getSpaceByLocation(randomSpace.location[0], randomSpace.location[1]);
+    $("[name=" + randomSpaceName + "]").attr("src", imageType);
+
+    setupNextMove();
+  }
+
+  function findRandomSpace(spaces)
+  {
+    var availableSpaces = []
+    for(var i = 0; i < spaces.length; i++)
     {
-        var team = spaces[0].player;
-        if( spaces[1].player === team && spaces[2].player === team )
-        {
-            return spaces[0].player;
-        }
-        
-        team = spaces[3].player;
-        if( spaces[4].player === team && spaces[5].player === team )
-        {
-            return spaces[3].player;
-        }
-        
-        team = spaces[6].player;
-        if( spaces[7].player === team && spaces[8].player === team )
-        {
-            return spaces[6].player;
-        }
-        
-        team = spaces[0].player;
-        if( spaces[3].player === team && spaces[6].player === team )
-        {
-            return spaces[0].player;
-        }
-        
-        team = spaces[1].player;
-        if( spaces[4].player === team && spaces[7].player === team )
-        {
-            return spaces[1].player;
-        }
-        
-        team = spaces[2].player;
-        if( spaces[5].player === team && spaces[8].player === team )
-        {
-            return spaces[2].player;
-        }        
-        
-        team = spaces[0].player;
-        if( spaces[4].player === team && spaces[8].player === team )
-        {
-            return spaces[0].player;
-        }
-        
-        team = spaces[6].player;
-        if( spaces[4].player === team && spaces[2].player === team )
-        {
-            return spaces[6].player;
-        }
-        
-        return -1;
+      if(spaces[i].player === -1)
+      {
+        availableSpaces.push(spaces[i]);
+      }
     }
+    return availableSpaces[Math.floor(Math.random() * availableSpaces.length)];
+  }
+
+  function checkForWinner(spaces)
+  {
+    var team = spaces[0].player;
+    // Check if top row belongs to one player
+    if(spaces[1].player === team && spaces[2].player === team)
+    {
+      return spaces[0].player;
+    }
+    // Check if middle row belongs to one player
+    team = spaces[3].player;
+    if(spaces[4].player === team && spaces[5].player === team)
+    {
+      return spaces[3].player;
+    }
+    // Check if bottom row belongs to one player
+    team = spaces[6].player;
+    if(spaces[7].player === team && spaces[8].player === team)
+    {
+      return spaces[6].player;
+    }
+    // Check if first column belongs to one player
+    team = spaces[0].player;
+    if(spaces[3].player === team && spaces[6].player === team)
+    {
+      return spaces[0].player;
+    }
+    // Check if middle column belongs to one player
+    team = spaces[1].player;
+    if(spaces[4].player === team && spaces[7].player === team)
+    {
+      return spaces[1].player;
+    }
+    // Check if last column belongs to one player
+    team = spaces[2].player;
+    if(spaces[5].player === team && spaces[8].player === team)
+    {
+      return spaces[2].player;
+    }
+    // Check if 0-9 diagonal belongs to one player
+    team = spaces[0].player;
+    if(spaces[4].player === team && spaces[8].player === team)
+    {
+      return spaces[0].player;
+    }
+    // Check if 2-6 diagonal belongs to one player
+    team = spaces[6].player;
+    if(spaces[4].player === team && spaces[2].player === team)
+    {
+      return spaces[6].player;
+    }
+    // If no conditions are met, return no player
+    return -1;
+  }
 });
-
--- spec/specs.js
+++ spec/specs.js
@@ -1,19 +1,18 @@
-
 describe( 'Tick Tac Toe', function() {
    it("Creates player", function() {
         var newPlayer = new Player( 0, "Player" );
-        expect( newPlayer.team ).to.equal( 0 ); 
+        expect( newPlayer.team ).to.equal( 0 );
         expect( newPlayer.name ).to.equal( "Player" );
    });
    it("Creates space", function() {
         var newSpace = new Space( 2, 2, 0 );
-        expect( newSpace.location[0] ).to.equal( 2 ); 
+        expect( newSpace.location[0] ).to.equal( 2 );
         expect( newSpace.location[1] ).to.equal( 2 );
         expect( newSpace.location[2] ).to.equal( 0 );
         expect( newSpace.player ).to.equal( -1 );
    });
    it("Creates board", function() {
         var newBoard = new Board( );
-        expect( newBoard.spaces.length ).to.equal( 9 ); 
+        expect( newBoard.spaces.length ).to.equal( 9 );
    });
 });
